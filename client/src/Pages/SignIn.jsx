2494a9adc43d6dae291213a1f99fbc7a7ba08607
Nitheesh-Achuthan <nitheeshachuthan05@gmail.com>
2024-07-05 12:50:32 +0530

    redux toolkit add

--- client/src/Pages/SignIn.jsx
+++ client/src/Pages/SignIn.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { Label, TextInput,Button, Alert, Spinner } from 'flowbite-react'
+import {useDispatch, useSelector} from 'react-redux'
+import { signInStart, signInSuccess,signInFailure } from '../redux/user/userSlice'
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading,setLoading] = useState(false);
+  const {loading, error:errorMessage} = useSelector(state=>state.user)
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleChange = (e) =>{
     setFormData({...formData, [e.target.id]: e.target.value.trim()})
@@ -13,11 +15,10 @@ export default function SignIn() {
   const handleSubmit = async (e) =>{
     e.preventDefault();
     if(!formData.email || !formData.password){
-        return setErrorMessage('Please fill out all fields');
+        return dispatch(signInFailure('Please fill all the fields'))
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+     dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {'Content-Type':'application/json'},
@@ -26,16 +27,15 @@ export default function SignIn() {
       const data = await res.json();
       console.log(data,'==========');
       if (data.success === false){
-        return setErrorMessage(data.message);
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false);
       if(res.ok){
+        dispatch(signInSuccess(data))
         navigate('/')
       }
       
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(signInSuccess(error.message));
     }
   }
   return (
@@ -90,7 +90,7 @@ export default function SignIn() {
               <Alert className='mt-5' color='failure'>
                 {errorMessage}
               </Alert>
-            )
+            ) 
           }
         </div>
       </div>

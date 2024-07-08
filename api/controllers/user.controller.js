5fe195a96cbb7e4498104d4b5c93e2d1ff7dff80
Nitheesh-Achuthan <nitheeshachuthan05@gmail.com>
2024-07-08 19:47:05 +0530

    add delete user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -45,7 +45,18 @@ export const updateUser = async (req,res,next) =>{
             res.status(200).json(rest);
         } catch (error) {
             next(error)
-        }
-        
+        }       
     
-}  
+};
+
+export const deleteUser = async(req,res,next)=>{
+    if(req.user.id !== req.params.userId) {
+        return next(errorHandler(403,'you are not allowed to delete this user'));
+    }
+    try {
+        await User.findByIdAndDelete(req.params.userId);
+        res.status(200).json('user has been deleted')
+    } catch (error) {
+        next(error);
+    }
+}

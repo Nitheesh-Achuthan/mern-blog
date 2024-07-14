4ebe2ca4efd89a87471384f4d02cebd7e99ec262
Nitheesh-Achuthan <nitheeshachuthan05@gmail.com>
2024-07-14 22:37:05 +0530

    add delete user to admin fuctionality

--- client/src/Components/DashUsers.jsx
+++ client/src/Components/DashUsers.jsx
@@ -47,21 +47,20 @@ export default function DashUsers() {
        }
     };
     const handleDeleteUser = async ()=>{
-            setShowModal(false);
+            
             try {
                 const res = await fetch(
-                    `/api/post/deletepost/${postIdToDelete}/${currentUser._id}`,
+                    `/api/user/delete/${userIdToDelete}`,
                     {
                         method: 'DELETE',
                     }
                 );
                 const data= await res.json();
-                if(!res.ok) {
-                    console.log(data.message);
+                if(res.ok) {
+                    setUsers((prev)=> prev.filter((user)=> user._id !== userIdToDelete));
+                    setShowModal(false);
                 } else {
-                    setUserPosts ((prev)=>
-                      prev.filter((post)=> post._id !== postIdToDelete)
-                    )
+                    console.log(data.message);                    
                 }
             } catch (error) {
                 console.log(error.message);
